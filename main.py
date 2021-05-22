9d02401950e80eb5ca746cbb3922078bc5d50e49
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-22 10:40:15 +0700

    create a post request to sheety api to add rows in spreadsheet

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 import requests
+from datetime import datetime
 import os
 load_dotenv()
 
+# NUTRITIONIX
 GENDER = 'male'
 WEIGHT_KG = 70
 HEIGHT_CM = 172
@@ -29,7 +31,34 @@ parameters = {
 }
 
 response = requests.post(url=url, headers=headers, json=parameters)
-print(response.json())
+exercises = response.json()['exercises']
+
+# SHEETY
+USERNAME = os.environ.get('SHEETY_USERNAME')
+PROJECT_NAME = os.environ.get('SHEETY_PROJECT_NAME')
+SHEET_NAME = os.environ.get('SHEETY_SHEET_NAME')
+BEARER = os.environ.get('BEARER')
+
+sheety_headers = {
+    'Authorization': f'Bearer {BEARER}',
+    'Content-Type': 'application/json'
+}
+
+date = datetime.now()
+sheety_url = f'https://api.sheety.co/{USERNAME}/{PROJECT_NAME}/{SHEET_NAME}'
+
+for exercise in exercises:
+    row_body = {
+        'workout': {
+            'date': date.strftime('%d/%m/%Y'),
+            'time': date.strftime('%-I:%M:%S %p'),
+            'exercise': exercise['name'].title(),
+            'duration': int(str(exercise['duration_min'])),
+            'calories': exercise['nf_calories']
+        }
+    }
+    sheet_response = requests.post(url=sheety_url, json=row_body, headers=sheety_headers)
+
 
 
 
